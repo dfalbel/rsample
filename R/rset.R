ddede51ff3091217c89a17142732987c64c1e463
Daniel Falbel <dfalbel@gmail.com>
2021-09-19 18:53:35 -0300

    rm fingerprint

--- R/rset.R
+++ R/rset.R
@@ -74,8 +74,8 @@ new_rset <-  function(splits, ids, attrib = NULL,
     res <- add_class(res, cls = subclass)
   }
 
-  fingerprint <- rlang::hash(res)
-  attr(res, "fingerprint") <- fingerprint
+  # fingerprint <- rlang::hash(res)
+  # attr(res, "fingerprint") <- fingerprint
 
   res
 }
